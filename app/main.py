d5b280a7b29aa36be5141631c7313a6e3773841b
Oscar Cuellar <oscar.cuellar@crearia.co>
2025-04-08 10:58:36 -0500

    Añadir estructura completa del proyecto

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from app.main import app
-import uvicorn
+from fastapi import FastAPI
+from app.api.endpoints import router
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+app = FastAPI(title="Agente de Voz")
+
+app.include_router(router)
