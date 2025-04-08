2f14cca48ed0e7203c26dcc9388274c78a19aaaf
Oscar Cuellar <oscar.cuellar@crearia.co>
2025-04-08 12:28:14 -0500

    Añadir estructura completa del proyecto

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from app.api.endpoints import router
+from fastapi.routing import APIRouter
 
 app = FastAPI(title="Agente de Voz")
 
-app.include_router(router)
+main_router = APIRouter()
+main_router.include_router(router, prefix="/agente-voz")
+
+app.include_router(main_router)
